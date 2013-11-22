7ee4ab3b445289a3a84f49718aecb4e65df9b35d
Saadia <sahmed@intertechmedia.com>
2013-11-21 23:24:25 -0500

    btn

--- scripts/mini.js
+++ scripts/mini.js
@@ -1,39 +1,65 @@
 
-<script>
+
 				$("document"). ready(function(){
-						
-					$(".guess_box"). click(CheckForCode);
-						
-						function CheckForCode(){
-							var my_num = Math.floor((Math.random()*5) +5);
-							var discount_msg = "<p>Your Discount is " + my_num + "%</p>";
-						//alert (discount_msg);
-							$(this).append(discount_msg);
-							
-							$(".guess_box"). each(function(){
-								$(this).unbind('click');
-							})
-							
-						
-						});// end of click function
-						
-				});//end ready function
+
+					$("#btn1").click function(){
+
+					$("#header").addClass("hover");
+					$("#header").removeClass("no_hover");	
+					}
+
+					$("#btn2").click function(){
 
 					
-				function getRandom(Num){
+					$("#header").removeClass("no_hover");
+					$("#header").addClass("hover");	
+					}
+
+						$("document"). ready(function(){
+					$(".guess_box").click(CheckForCode);
+					
+					function getRandom(Num){
 					var my_num=Math.floor((Math.random()*Num) +5);
 					return my_num;
 				}
 
-				function hideCode(){
-					var numRand=getRandom(4);
-					$.(".guess_box").each(function(index,value){
+
+				var hideCode = function (){
+
+					var numRand = getRandom(4);
+					$(".guess_box").each(function(index,value){
 						if(numRand==index){
 							$(this).append("<span id='has_discount'></span>");
 							return false;
 						}
 
+					});
 				}
 
+					hideCode();	
 
-</script>
+					function CheckForCode(){
+					var Discount;
+					if ($.contains(this, document.getElementById("has_discount")))
+					{
+					var my_num=getRandom(5); 
+					discount="<p>Your Discount is " + my_num + "%</p>";
+					}
+					else {
+					discount="<p>No Discount rt now " + my_num + "%</p>";	
+					}
+					$(this).append(discount);
+								
+								$(".guess_box"). each(function(){
+									$(this).unbind('click');
+					});
+
+				}
+
+			});	
+
+				
+
+			
+
+
