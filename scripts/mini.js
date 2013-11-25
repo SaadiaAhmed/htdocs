3b14797e2b6fd852249e390c36db2adcdd6a8036
Saadia <sahmed@intertechmedia.com>
2013-11-24 23:18:07 -0500

    joy

--- scripts/mini.js
+++ scripts/mini.js
@@ -1,65 +1,56 @@
+	$("document").ready(function(){
+	
+		$(".guess_box").click(CheckForCode);
+		function getRandom(num){
+		var my_num = Math.floor(Math.random()*num);
+		return my_num;}
+	
 
 
-				$("document"). ready(function(){
+	var hideCode = function houdini(){
+		var numRand = getRandom(4);
+		$(".guess_box").each(function(index, value){
+		if(numRand == index){
+		$(this).append("<span id='has_discount'></span>");
+		return false;
+		}
+		});
+		}
 
-					$("#btn1").click function(){
+hideCode();	
 
-					$("#header").addClass("hover");
-					$("#header").removeClass("no_hover");	
-					}
-
-					$("#btn2").click function(){
-
-					
-					$("#header").removeClass("no_hover");
-					$("#header").addClass("hover");	
-					}
-
-						$("document"). ready(function(){
-					$(".guess_box").click(CheckForCode);
-					
-					function getRandom(Num){
-					var my_num=Math.floor((Math.random()*Num) +5);
-					return my_num;
-				}
-
-
-				var hideCode = function (){
-
-					var numRand = getRandom(4);
-					$(".guess_box").each(function(index,value){
-						if(numRand==index){
-							$(this).append("<span id='has_discount'></span>");
-							return false;
-						}
-
-					});
-				}
-
-					hideCode();	
-
-					function CheckForCode(){
-					var Discount;
+	function CheckForCode(){
+					var discount;
 					if ($.contains(this, document.getElementById("has_discount")))
 					{
-					var my_num=getRandom(5); 
+					var my_num=getRandom(100); 
 					discount="<p>Your Discount is " + my_num + "%</p>";
 					}
 					else {
 					discount="<p>No Discount rt now " + my_num + "%</p>";	
 					}
-					$(this).append(discount);
-								
-								$(".guess_box"). each(function(){
-									$(this).unbind('click');
-					});
+					
+					$(".guess_box").each(function() { 
+							if($.contains(this, document.getElementById("has_discount") ) )
+							{
+								$(this).addClass("discount");
+							}else{
+								$(this).addClass("no_discount");            
+							}
+							$(this).unbind();
+						});
+					$("#result").append(discount);	
 
 				}
-
-			});	
-
 				
-
+			$("guess_box").hover(
+			function (){
+				$(this).addClass("my_hover");
+				};
 			
-
+			function (){
+				$(this).removeClass("my_hover");
+				};
+			);
+});
 
