0740b89bf6dba09371583c432f3392e9b194340f
Saadia <sahmed@intertechmedia.com>
2013-11-27 17:19:06 -0500

    menu

---
+++ scripts/menu.js
@@ -0,0 +1,24 @@
+	$("document").ready(function(){
+	
+		var v=f;
+		$("button#Veg_On").click(function(){
+
+			if (v == false){
+				v=true;
+			}
+
+		} )
+		
+$("button#restore_Me").click(function(){
+
+			if (v == true){
+				v=false;
+			}
+
+		} )
+		
+
+
+		});
+		
+
