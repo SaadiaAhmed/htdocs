f27905d300e51cda9b7e20dd1ade9f7c3dfab6b5
Saadia <sahmed@intertechmedia.com>
2013-12-04 16:26:54 -0500

    menu 2

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,21 +1,44 @@
-	$("document").ready(function(){
+	$(document).ready(function(){
 	
-		var v=f;
+		var v = false;
+		var $f, $m;
+
 		$("button#Veg_On").click(function(){
 
 			if (v == false){
-				v=true;
-			}
 
-		} )
+				$f = $(".fish").parent().parent().detach();
+				$(".hamburger").replaceWith("<li class='portobello'><em>Portobello Mushroom</em></li>");	
+				$(".portobello").parent().parent().addClass("veg_leaf");
+				$(".meat").after("<li class='tofu'><em>tofu</em></li>");
+				$m = $(".meat").detach();
+				$(".tofu").parent().parent().addClass("veg_leaf");
+				v = true;
+			}
+			
+		} );
 		
 $("button#restore_Me").click(function(){
 
 			if (v == true){
-				v=false;
+
+			$(".portobello").parent().parent().removeClass("veg_leaf");
+			$(".portobello").replaceWith("<li class='hamburger'><em>hamburger</em></li>");
+			$(".tofu").parent().parent().removeClass("veg_leaf");
+			
+			$(".menu_entrees li").first().before($f);
+			$(".tofu").each(function (i){
+			$(this).after($m[i]);
+
+			});
+
+			$(".tofu").remove();
+			v = false;
+
+				
 			}
 
-		} )
+		} );
 		
 
 
