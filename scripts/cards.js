488270dc94e554581277df0c7113ff461a49abb0
Saadia <sahmed@intertechmedia.com>
2013-12-10 17:58:50 -0500

    cards

---
+++ scripts/cards.js
@@ -0,0 +1,31 @@
+$(document).ready (function (){
+
+function card (name,suit,value){
+
+this.name = name;
+this.suit = suit;
+this. value = value;
+}
+
+
+var deck = [
+
+new card('Ace', 'Hearts', 1);
+new card('Two', 'Hearts', 2);
+new card('Three', 'Hearts', 3);
+new card('Four', 'Hearts', 4);
+new card'Five', 'Hearts', 5);
+new card('Six', 'Hearts', 6);
+new card('Seven', 'Hearts', 7);
+new card('Eight', 'Hearts', 8);
+new card('Nine', 'Hearts', 9);
+new card('Ten', 'Hearts', 10);
+new card('Jack', 'Hearts',J),
+new card('Queen', 'Hearts',Q),
+new card('King', 'Hearts',K)
+
+]
+
+
+
+});
