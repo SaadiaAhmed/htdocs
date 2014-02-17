ee088ecc48ef09ffc3d04b373f61c16a8e1e8404
Saadia <sahmed@intertechmedia.com>
2014-02-16 22:51:16 -0500

    race5

--- scripts/race.js
+++ scripts/race.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 
  var  FREQ = 10000;
-function startAJAXcalls(){
+ var  repeat = true;
 
-setTimeout( function(){
+function showFrequency(){
+$('#freq').html("Page Refreshes every "+ FREQ/1000 + "seconds  ");
+}
+
+function startAJAXcalls(){
+if(repeat){
+setTimeout(function(){
 getXMLracers();
 startAJAXcalls();
 
 },FREQ);
+}
   } 
    
 
@@ -43,34 +50,50 @@ $(xml).find("runner").each(function (){
 
     });
 
-
-getTime();
+getTimeAjax();
+//showFrequency();
+//getTime();
 }
 
 
 });
 
 }
+function getTimeAjax(){
+    $('#updatedTime').load("/PHP/time.php");
+}
+
+
+$("#Stop").click(function(){
+    repeat = false;
+$('#freq').html("UDATES paused");
+});
+
+$("#Start").click(function(){
+    repeat = true;
+startAJAXcalls();
+showFrequency();
+ });
 
 //startAJAXcalls();
 
-	function getTime(){
-        var a_p = "";
-        var d = new Date();
-        var curr_hour = d.getHours();
+	//function getTime(){
+       //////////////////////// var a_p = "";
+       ////////////////////// var d = new Date();
+       //////////////////// var curr_hour = d.getHours();
         
-        (curr_hour < 12) ? a_p = "AM" : a_p = "PM";
-        (curr_hour == 0) ? curr_hour = 12 : curr_hour = curr_hour;
-        (curr_hour > 12) ? curr_hour = curr_hour - 12 : curr_hour = curr_hour;
+        //////////////////(curr_hour < 12) ? a_p = "AM" : a_p = "PM";
+        ////////////////(curr_hour == 0) ? curr_hour = 12 : curr_hour = curr_hour;
+        //////////////(curr_hour > 12) ? curr_hour = curr_hour - 12 : curr_hour = curr_hour;
         
-        var curr_min = d.getMinutes().toString();
-        var curr_sec = d.getSeconds().toString();
+        ////////////var curr_min = d.getMinutes().toString();
+        //////////var curr_sec = d.getSeconds().toString();
         
-        if (curr_min.length == 1) { curr_min = "0" + curr_min; }
-        if (curr_sec.length == 1) { curr_sec = "0" + curr_sec; } 
+        ////////if (curr_min.length == 1) { curr_min = "0" + curr_min; }
+        //////if (curr_sec.length == 1) { curr_sec = "0" + curr_sec; } 
         
-        $('#updatedTime').html(curr_hour + ":" + curr_min + ":" + curr_sec + " " + a_p );
-    }
-//getXMLRacers();
-//startAJAXcalls();
+        ////$('#updatedTime').html(curr_hour + ":" + curr_min + ":" + curr_sec + " " + a_p );
+   // }
+startAJAXcalls();
+showFrequency();
 });
