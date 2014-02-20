00eb34416b0294585b6c8657870e775bfa989261
Saadia <sahmed@intertechmedia.com>
2014-02-20 17:28:31 -0500

    race 9

--- scripts/race.js
+++ scripts/race.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
- var  FREQ = 10000;
- var  repeat = true;
+ var FREQ = 10000;
+ var repeat = true;
 
 function showFrequency(){
-$('#freq').html("Page Refreshes every "+ FREQ/1000 + "seconds  ");
+$('#freq').html("Page Refreshes every "+ FREQ/1000 + "seconds ");
 }
 
 function startAJAXcalls(){
@@ -12,19 +12,15 @@ if(repeat){
 setTimeout(function(){
 getXMLracers();
 startAJAXcalls();
-
-},FREQ);
+}, FREQ);
 }
-  } 
+  }
    
-
-getXMLracers();
-startAJAXcalls();
 
 function getXMLracers(){
 
+
 $.ajax({
-//type: "GET",
 url:"http://localhost/XML/runners.xml",
 cache:false,
 datatype:"xml",
@@ -38,7 +34,7 @@ $(xml).find("runner").each(function (){
  var info = '<li> Name: '+ $(this).find("fname").text() + ' '+ $(this).find("lname").text() + '. Time: '+ $(this).find("time").text() + '</li>';
 
  if( $(this).find("gender").text() == "m" ){
-  $("#finishers_m").append(info); 
+  $("#finishers_m").append(info);
     }
 
   else if( $(this).find("gender").text() == "f"){
@@ -59,8 +55,9 @@ getTimeAjax();
 });
 
 }
+
 function getTimeAjax(){
-    $('#updatedTime').load("/PHP/time.php");
+    $("#updatedTime").load("http://localhost/PHP/time.php");
 }
 
 
@@ -69,31 +66,16 @@ $("#Stop").click(function(){
 $('#freq').html("UDATES paused");
 });
 
-$("#Start").click(function(){
-    repeat = true;
-startAJAXcalls();
-showFrequency();
- });
+$("#btnStart").click(function(){
+ repeat = true;
+  startAJAXcalls();
+    showFrequency();
+});
 
-//startAJAXcalls();
 
-	//function getTime(){
-       //////////////////////// var a_p = "";
-       ////////////////////// var d = new Date();
-       //////////////////// var curr_hour = d.getHours();
-        
-        //////////////////(curr_hour < 12) ? a_p = "AM" : a_p = "PM";
-        ////////////////(curr_hour == 0) ? curr_hour = 12 : curr_hour = curr_hour;
-        //////////////(curr_hour > 12) ? curr_hour = curr_hour - 12 : curr_hour = curr_hour;
-        
-        ////////////var curr_min = d.getMinutes().toString();
-        //////////var curr_sec = d.getSeconds().toString();
-        
-        ////////if (curr_min.length == 1) { curr_min = "0" + curr_min; }
-        //////if (curr_sec.length == 1) { curr_sec = "0" + curr_sec; } 
-        
-        ////$('#updatedTime').html(curr_hour + ":" + curr_min + ":" + curr_sec + " " + a_p );
-   // }
-startAJAXcalls();
+  
 showFrequency();
+getXMLracers();
+startAJAXcalls();
+
 });
