e7f374bb9103d61dccc31dab0309e42c1636c026
Saadia <sahmed@intertechmedia.com>
2014-02-24 22:29:28 -0500

    race12

--- PHP/service.php
+++ PHP/service.php
@@ -1,6 +1,8 @@
 <?php
 
-date_default_timezone_set('America/Los_Angeles');
-echo date("F j, Y, g:i:s a");
+mysql_connect('127.0.0.1', 'root','logan123')
+OR die('could not connect to database');
+mysql_select_db('runner_info');
 
+echo "connected!";
 ?>
