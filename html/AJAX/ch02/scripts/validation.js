96ffd043fe4cbb65021c9b800239202086fc11a8
Saadia <sahmed@intertechmedia.com>
2014-04-07 17:23:27 -0400

    ch2

---
+++ html/AJAX/ch02/scripts/validation.js
@@ -0,0 +1,18 @@
+
+
+window.onload = initPage;
+
+	function initPage(){
+	
+		document.getElementById('username').onblur = checkUserName;
+}
+
+
+	function checkUserName(){
+	alert("iooi");
+	}
+	function showUserNameStaus(){
+	alert("showUserNameStaus()");
+
+		
+	}
